4ccfc5571ad0cc1cc12704cb44d0be76f03c1edd
Ke Wang <46043861+KAIwangke@users.noreply.github.com>
2024-05-07 20:50:57 -0400

    1

--- fastMoE/examples/megatron/Megatron-LM-2.5/pretrain_gpt_distributed_with_mp.sh
+++ fastMoE/examples/megatron/Megatron-LM-2.5/pretrain_gpt_distributed_with_mp.sh
@@ -48,7 +48,6 @@ torchrun $DISTRIBUTED_ARGS \
        --save-interval 10000 \
        --eval-interval 1000 \
        --eval-iters 10 \
-       --local_rank 2\
        --num-experts 4\
        --fp16
 
