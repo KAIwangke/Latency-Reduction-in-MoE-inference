bb68e9f725e8d9808db8829df07fe417133f6a70
Ke Wang <46043861+KAIwangke@users.noreply.github.com>
2024-05-10 18:04:59 -0400

    catch smart forward

--- fastMoE/fmoe/fastermoe/schedule.py
+++ fastMoE/fmoe/fastermoe/schedule.py
@@ -59,7 +59,7 @@ class MoEForward(Function):
         def stash_fn(params, store_idx, expert_idx):
             expert_utils.stash_expert_params(experts, params, expert_idx)
             ctx.shadows[store_idx] = params
-
+        print("!!!!"*50)
         local_output_buf, gib = fmoe_native.smart_sch_forward(
                 local_input_buf,
                 local_expert_count, global_expert_count, 
