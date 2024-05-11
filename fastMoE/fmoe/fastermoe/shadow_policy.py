b521272f45a519d5af5236e040b027fc4b1ad4ec
Ke Wang <46043861+KAIwangke@users.noreply.github.com>
2024-05-11 01:22:05 -0400

    1

--- fastMoE/fmoe/fastermoe/shadow_policy.py
+++ fastMoE/fmoe/fastermoe/shadow_policy.py
@@ -70,8 +70,8 @@ def global_policy(local_expert_count, _gec, num_expert, world_size):
 
 
     print("the updated res")
-    print(res)
     res[0] = True
+    print(res)
     # shadowed_experts = torch.nonzero(res).flatten().tolist()
     # print(f"Shadowed experts: {shadowed_experts}")
             
