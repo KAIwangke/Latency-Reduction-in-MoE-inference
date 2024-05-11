036bd40ac2027b9496c57ef7473777becd41dcc5
Ke Wang <46043861+KAIwangke@users.noreply.github.com>
2024-05-11 00:47:22 -0400

    1

--- fastMoE/fmoe/fastermoe/shadow_policy.py
+++ fastMoE/fmoe/fastermoe/shadow_policy.py
@@ -28,11 +28,12 @@ def global_policy(local_expert_count, _gec, num_expert, world_size):
     agecs = [torch.empty_like(local_expert_count) for _ in range(world_size)]
     dist.all_gather(agecs, local_expert_count, group=moe_group)
     all_global_expert_count = torch.stack(agecs)
-
     # TODO: data type other than float
     data_size = 4 
 
     fwd_expert_counts = all_global_expert_count.sum(1).cpu()
+    print(all_global_expert_count)
+    print(fwd_expert_counts.flatten())
     
     B_ws, indices = fwd_expert_counts.flatten().sort(0, descending=True)
 
@@ -46,8 +47,8 @@ def global_policy(local_expert_count, _gec, num_expert, world_size):
     lat_base = 3 * comp_time * B_w + 4 * send_feature_time * B_w
 
     res = torch.zeros(world_size * num_expert, dtype=torch.bool)
-    print("lets check res")
-    print(res)
+    # print("lets check res")
+    # print(res)
 
 
     shadow_time = 0
@@ -67,8 +68,8 @@ def global_policy(local_expert_count, _gec, num_expert, world_size):
             break
 
 
-    print("the updated res")
-    print(res)
+    # print("the updated res")
+    # print(res)
     # shadowed_experts = torch.nonzero(res).flatten().tolist()
     # print(f"Shadowed experts: {shadowed_experts}")
             
